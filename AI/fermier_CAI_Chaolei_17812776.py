68e92563dba91647820fd925a0b21135cbd5fd9a
CAI Chaolei <bk211@localhost>
2018-10-02 14:24:35 +0200

    ??pb commit

--- AI/fermier_CAI_Chaolei_17812776.py
+++ AI/fermier_CAI_Chaolei_17812776.py
@@ -59,15 +59,20 @@ def recherche_aleatoire():
 	while (curseur != etat_but):
 		print(f'>>{curseur} coup numero :{nb_de_coup}')
 		etat_disponible = traverse_disponible(curseur)
-		etat_disponible=rejet_etat_double(etat_disponible)
-		print(f'\t{etat_disponible}\n')
+		#la liste ED ci dessus est affecte des successeurs possible du curseur
+		etat_disponible=rejet_etat_double(etat_disponible)#sert a rien normalement
+		print(f'\tetat disponible: {etat_disponible}\n')
+		etats_deja_visite.append(copier(curseur))#sert a rien aussi
 		curseur = etat_disponible.pop(random.randrange(len(etat_disponible)))
+		#on tire au sort un etat aleatoire depuis ED
+		#qui est aussitot enlever d'ED par la methode pop
 		nb_de_coup +=1
-		etats_deja_visite.append(copier(curseur))
 	print(f'>>{curseur} coup numero :{nb_de_coup}')
 	print(f'fin de jeu')
 
 def recherche_profondeur():
+	#miracle ca fonctionne car bizarrement la solution est parcourue en prenant tjrs la fourche gauche
+	#donc cette algo est bidon...
 	curseur = etat_init
 	nb_de_coup = 0
 	global etat_disponible
@@ -75,24 +80,51 @@ def recherche_profondeur():
 	while (curseur != etat_but):
 		print(f'>>{curseur} coup numero :{nb_de_coup}')
 		etat_disponible = traverse_disponible(curseur)
-		etat_disponible=rejet_etat_double(etat_disponible)
-		print(f'\t{etat_disponible}\n')
-		curseur = etat_disponible[0]
+		etat_disponible =rejet_etat_double(etat_disponible)
+		etat_disponible =rejet_etat_visite(etat_disponible)
+		print(f'\tetat disponible: {etat_disponible}\n')
+		curseur = etat_disponible[0]# vive la fourche gauche
 		nb_de_coup +=1
 		etats_deja_visite.append(copier(curseur))
-	print(f'>>{curseur} coup numero :{nb_de_coup}')
+	print(f'>>{curseur} coup numero :{nb_de_coup}')	
 	print(f'fin de jeu')
 
-recherche_profondeur()
+def recherche_largeur():
+	curseur = etat_init
+	nb_de_coup = 0
+	global etat_disponible
+	global etats_deja_visite
+	etat_disponible_pr_curseur = []
+	while (curseur != etat_but):
+		print(f'>>{curseur} coup numero :{nb_de_coup}')
+		etat_disponible_pr_curseur = traverse_disponible(curseur)	
+		print(f'\tetat etat_disponible_pr_curseur: {etat_disponible_pr_curseur}')	
+		#EDPC ci dessus est une sorte de pile temporaire
+		#ED contient tous les etats non parcourue d'une meme niveau a priori
+		if(not len(etat_disponible_pr_curseur)):
+			#si on trouve pas d'etat successeur on reviens vers ED pour prendre un autre curseur
+			curseur = etat_disponible[0]
+		elif(len(etat_disponible_pr_curseur)==1):
+			#le cas ou il n'y a qu'un seul succeusseur
+			#on peut donc mettre le curseur dans le bac des visite, pour qui soit sortie de ED
+			etats_deja_visite.append(copier(curseur))
+			curseur = etat_disponible_pr_curseur[0]
+		else:
+			#si on a plusieurs successeurs on ajoute leurs successeurs dans ED
+			for elem in etat_disponible_pr_curseur:
+				etat_disponible += traverse_disponible(elem)
+			etats_deja_visite.append(copier(curseur))
+			curseur = etat_disponible[0]
+		#enfin on tri le bac ED en enlevant tous les doublons et les visitees
+		#et on boucle jusqu'a tomber sur la solution
+		etat_disponible =rejet_etat_double(etat_disponible)
+		etat_disponible =rejet_etat_visite(etat_disponible)
+		print(f'\tetat disponible: {etat_disponible}\n')	
+		nb_de_coup +=1
+
+	print(f'>>{curseur} coup numero :{nb_de_coup}')	
+	print(f'fin de jeu')
 
 #recherche_profondeur()
-#rejet_etat_interdit(etat_but)
-#test = [[0,1,1,0],[1,1,1,1],[0,1,1,0],[1,0,0,1]]
-#print(test)
-#test = rejet_etat_interdit(test)
-#print(test)
-
-#print(etat_disponible)
-
-#print([1,0,1,0] in etats_interdit)
-#print(etat_disponible in etats_interdit)
+#recherche_largeur()
+#recherche_aleatoire()

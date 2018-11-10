a7547f19040e4f81462807fd123bb40f036d1329
Playmaker <chaoleicai@gmail.com>
2018-11-10 11:38:46 +0100

    creation TP6

---
+++ POO/TP6/FilActualites.java
@@ -0,0 +1,11 @@
+public class FilActualites {
+// Completer avec variables necessaires
+ /∗∗ Construit un fil vide ∗/
+ public FilActualites () { // A completer } /∗∗ Ajoute une publication au fil . ∗/
+public void ajoutePost(Post post) { // A completer }
+/∗∗ Affiche tous les posts , leur auteur + nb de votes ∗/
+public void affiche () { // A completer }
+/∗∗ Affiche le nombre de posts publiés par l’auteur de nom a ∗/
+public int nombrePostAuteur(String a) { // A completer }
+/∗∗ Totalité des votes pour les publications dans ce fil ∗/
+public int popularite () { // A completer } }
---
+++ POO/TP6/Post.java
@@ -0,0 +1,20 @@
+public class Post {
+private String auteur ;
+private int likes; // votes sur ce post
+//Constructeur
+public Post ( String auteur ){
+auteur = a;
+likes = 0; }
+public String getAuteur (){ return auteur ; }
+public int getLikes (){ return likes ; }
+//Incrémente votes "j’aime" sur le post
+public void Aime(){ likes++; }
+public void AimePas (){
+  if (likes > 0) {likes−−; }
+}
+//Affiche les details de ce post 
+public void affiche (){
+System.out.println ( auteur );
+if(likes > 0) {
+System.out.println(" − " + likes + " aiment."); }
+     } }

2e2fbb2527171d64ca673124607579ff5523c8fe
CAI Chaolei <bk211@octo>
2018-11-21 15:41:18 +0100

    creation tp8

---
+++ PIUM/TP8/tp8/plusloin/chemin_sinueux/chemin_montant/tout/au/bout/du/grosse_horloge.sh
@@ -0,0 +1,6 @@
+for i in `seq 1 100`; do
+    echo "TIC"
+    sleep 1
+    echo "TAC"
+    sleep 15
+done
---
+++ PIUM/TP8/tp8/plusloin/chemin_sinueux/chemin_montant/tout/au/bout/du/petite_horloge.sh
@@ -0,0 +1,6 @@
+for i in `seq 1 100`; do
+    echo "tic"
+    sleep 1
+    echo "tac"
+    sleep 10
+done

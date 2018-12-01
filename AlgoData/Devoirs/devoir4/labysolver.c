f59c4e5a42bfd774d8b5596c4cde365a745bd3a5
Playmaker <chaoleicai@gmail.com>
2018-12-01 19:23:49 +0100

    scan entree ok, fct exploration + print resultat a faire

--- AlgoData/Devoirs/devoir4/labysolver.c
+++ AlgoData/Devoirs/devoir4/labysolver.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 static int * labyParse(void) {
 	char buf[1024], * s;
 	int *labyrinthe = NULL, w = 0, h = 0, x, y;
@@ -6,13 +9,41 @@ static int * labyParse(void) {
 	/* ... */
 	y = 0;
 	/* allouer la m´emoire */
-	while(fgets(buf, sizeof buf, stdin)) {
+	labyrinthe = malloc(sizeof labyrinthe * w * h);
+
+	while(fgets(buf, sizeof buf, stdin)) {//line entiere lue depuis stdin stoké dans buf 
 		x = 0;
-		for(s = buf, x = 0; *s && *s != ’\n’; s++, x++) {
-		/* remplir labyrinthe et fixer le point de d´epart et le point
-		* d’arriv´ee */
+		for(s = buf, x = 0; *s && *s != '\n'; s++, x++) {
+		/* remplir labyrinthe et fixer le point de d´epart et le point d’arriv´ee */
+			//printf("%c\n",*s );
+			if(*s == '*') //mur 
+				labyrinthe[x + y*w]= -1;
+			else if(*s == ' ')//ouvert
+				labyrinthe[x + y*w]= 0;
+			else if(*s == 'D')//depart
+				labyrinthe[x + y*w]= -2;
+			else if(*s == 'A')//arrive
+				labyrinthe[x + y*w]= -3;
 		}
 		y++;
 	}
 	return labyrinthe;
+}
+
+void printlaby(int * t, int w, int h){
+	int i;
+	for(i = 0;i < w*h;++i){
+		if(i%w ==0 ) printf("\n");
+		printf("%d, ",t[i]);
+	}
+}
+//echo "4\n4\n****\n*A *\n* D*\n****" | ./labysolver 
+
+
+int main(int argc, char const *argv[])
+{
+	int * temp = labyParse();
+	printlaby(temp,4,4);
+	free(temp);
+	return 0;
 }

e12683073445c491a9143af65c94bfa5916a53ad
BK211 <chaoleicai@gmail.com>
2018-12-03 09:33:39 +0100

    revision fini

--- LearningC/TD_Descartes/TD7/exo.c
+++ LearningC/TD_Descartes/TD7/exo.c
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[])
 	printf("*P+2=%d\n",*P+2 );//34 non 14
 	printf("*(P+2)%d\n",*(P+2) );//34
 	printf("A+3=%d\n",A+3);//null
-	printf("&A[7]=%d\n",&A[7]);//89 non null
-	printf("&A[4]-3%d\n",&A[4]-3);
+	printf("&A[7]=%d\n",&A[7]);
+	printf("&A[4]-3=%d\n",&A[4]-3);
 	printf("A[5]=%d\n",A[5]);
 }
--- POO/TD/tryCatch.java
+++ POO/TD/tryCatch.java
@@ -1,37 +1,60 @@
-class tryCatch{
+class tryCatch
+{
 
-	static void methode1(int p) throws Exc1, Exc2{
-		System.out.println("Debut d’execution de methode1");
-		if (p == 0){
-			throw new Exc1 (); }
-		System.out.println("Milieu d’execution de methode1"); 
-		if(p ==1){
-			throw new Exc2 (); }
-		System.out.println("Fin d’execution de methode1"); }
-	
-	static void methode2(int p) throws Exc1, Exc2, Exc3{
-		System.out.println("Debut d’execution de methode2");
-		if (p == 2){
-			throw new Exc3(); }
-		System.out.println("Milieu d’execution de methode2");
-		methode1(p);
-		System.out.println("Fin d’execution de methode2"); }
+    static void methode1(int p) throws Exc1, Exc2
+    {
+        System.out.println("Debut d’execution de methode1");
+        if (p == 0)
+        {
+            throw new Exc1 ();
+        }
+        System.out.println("Milieu d’execution de methode1");
+        if(p == 1)
+        {
+            throw new Exc2 ();
+        }
+        System.out.println("Fin d’execution de methode1");
+    }
 
-	static void methode3(int p) throws Exc1, Exc3{
-			System.out.println("Debut d’execution de methode3");
-		
-		try{
-			methode2(p); }
-		catch(Exc2 ex){
-			System.out.println("Debut d’execution de methode3"); }
-		System.out.println("Fin d’execution de methode3"); }
-	
+    static void methode2(int p) throws Exc1, Exc2, Exc3
+    {
+        System.out.println("Debut d’execution de methode2");
+        if (p == 2)
+        {
+            throw new Exc3();
+        }
+        System.out.println("Milieu d’execution de methode2");
+        methode1(p);
+        System.out.println("Fin d’execution de methode2");
+    }
 
-	public static void main(String[] args) throws Exc1, Exc3{
-		System.out.println("Entrez un nombre: ");
-		methode3(2);
-	}}
+    static void methode3(int p) throws Exc1, Exc3
+    {
+        System.out.println("Debut d’execution de methode3");
 
-class Exc1 extends Exception{}
-class Exc2 extends Exception{}
-class Exc3 extends Exception{} 
+        try
+        {
+            methode2(p);
+        }
+        catch(Exc2 ex)
+        {
+            System.out.println("Debut d’execution de methode3");
+        }
+        catch(Exc3 adwjiaod){
+         System.out.println("Dejiafoewjdoawijdoawidj");
+           
+        }
+        System.out.println("Fin d’execution de methode3");
+    }
+
+
+    public static void main(String[] args) throws Exc1, Exc3
+    {
+        System.out.println("Entrez un nombre: ");
+        methode3(2);
+    }
+}
+
+class Exc1 extends Exception {}
+class Exc2 extends Exception {}
+class Exc3 extends Exception {}

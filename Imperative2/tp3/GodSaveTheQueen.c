3b560de7a65397eb2083640cece9eee5d216084e
playmaker <chaoleicai@gmail.com>
2019-10-08 10:57:47 +0200

    up correction exo2

--- Imperative2/tp3/GodSaveTheQueen.c
+++ Imperative2/tp3/GodSaveTheQueen.c
@@ -1,8 +1,37 @@
-#include <stdio>
+#include <stdio.h>
 #include <setjmp.h>
+#include <assert.h>
+#include <unistd.h>
+#include <signal.h>
 
-jmp_buf jbuf,j2;
+struct vec_int{
+    int nbele;
+    int * v;
+}
+typedef struct vec_int vec_int_t;
 
+vec_int_t v;
+void afficharec(vec_int_t w){
+    int i;
+    for ( i = 0; i < nbele; i++) {
+        printf("%d\n", w.w[i]);
+        printf("\n", );
+    }
+}
+
+void sig_handler(int signo){
+    int i;
+    FILE * fic;
+    if (signo == SIGINT) {
+        printf("recu SIGINT\n");
+        fic = fopen("FIB.txt", "a");
+        for (i = 0; i < v.nbele; i++) {
+            fprintf(fic, "%d\n",w.vpi );
+            fprintf(fic, "%s\n");
+            fclose(fic);
+        }
+    }
+}
 
 int main(int argc, char const *argv[]) {
     int a, b, q;

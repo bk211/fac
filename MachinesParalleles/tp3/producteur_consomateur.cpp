d404de7c188cb50387b377d867d95f64e7be6380
playmaker <chaoleicai@gmail.com>
2019-10-09 18:46:26 +0000

    add consomateurs function

--- MachinesParalleles/tp3/producteur_consomateur.cpp
+++ MachinesParalleles/tp3/producteur_consomateur.cpp
@@ -4,41 +4,47 @@
 #include <unistd.h>
 #include <mutex>
 #include <stdlib.h>
+#include <chrono>
 
 std::mutex mymutex;
 
 #define SLEEP_TIME 2
 #define MAX_POP_TIME 10
+#define MAX_PRODUCT 30
+#define NB_CUST 5
 
 void producteur(std::vector<int> *v, int* index);
-void consomateur(std::vector<int> *v,int* index);
+void consomateur(std::vector<int> *v,int* index, int n);
+void consomateurs(std::vector<int> *v,int* index);
 
 void _sleep_for(){
     int t = rand() % SLEEP_TIME + 1; //sleep entre 1 et SLEEP_TIME
-    std::cout << "Sleeping for " << t<< " secs\n";
-    sleep(t);
+    std::cout << "Sleeping for " << t<< " secs\n\n";
+    std::this_thread::sleep_for(std::chrono::seconds(t));
 }
-
 
 int main(int argc, char const *argv[]) {
     srand (time(NULL));
     std::vector<int> panier;
     int index = 0;
     std::thread t1(producteur,&panier, &index);
-    std::thread t2(consomateur,&panier, &index);
-
-    t2.join();
+//    std::thread t2(consomateur,&panier, &index, 0);
+//t2.join();
+    consomateurs(&panier, &index);
     t1.join();
     return 0;
 }
 
 
 void producteur(std::vector<int>* v, int *index){
-    for (int i = 0; *index <10 ; i++) {
+    for (int i = 0; *index < MAX_PRODUCT ; i++) {
         mymutex.lock();
         v->push_back(i);
+        v->push_back(i);
+        v->push_back(i);
+        v->push_back(i);
         *index +=1;
-        std::cout << "Prod: pushed " << i<< " au panier" <<", Il contient "<< *index<<" elements \n";
+        std::cout << "Prod: pushed <" << i<< "> au panier" <<", Il contient "<< *index<<" elements \n";
         //for(auto k: *v){ std::cout << k << " ,";}
         //std::cout << '\n';
         mymutex.unlock();
@@ -47,17 +53,17 @@ void producteur(std::vector<int>* v, int *index){
     std::cout << "End of prod" << '\n';
 }
 
-void consomateur(std::vector<int>* v,int *index){
+void consomateur(std::vector<int>* v,int *index, int n){
     int x;
     int npop = 0;
-    sleep(1);
+    sleep(2);
     //std::cout << "wake up" << '\n';
     while (*index>0) {
         mymutex.lock();
         x = v->back();
         v->pop_back();
         *index -=1;
-        std::cout << "Cust: pop " << x << " au panier"<<", Il contient "<< *index<<" elements \n";
+        std::cout << "Cust "<< n<<" pop <"<< x << "> au panier"<<", Il contient "<< *index<<" elements \n";
         //for(auto k: *v){ std::cout << k << " ,";}
         mymutex.unlock();
         npop++;
@@ -69,4 +75,15 @@ void consomateur(std::vector<int>* v,int *index){
     }
     std::cout << "End of cust" << '\n';
 
+}
+
+void consomateurs(std::vector<int> *v,int* index){
+    std::vector<std::thread> vec_cust;
+    for (int i = 0; i < NB_CUST; i++) {
+        vec_cust.push_back(std::thread (consomateur, v, index, i));
+    }
+
+    for ( auto& cust : vec_cust ) {
+        cust.join();
+    }
 }

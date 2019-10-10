1786b782b78bb218ebe8822cf1c2cd96168dfc52
playmaker <chaoleicai@gmail.com>
2019-10-10 15:31:27 +0000

    produit matrice a corriger

--- POO/tp2/Main.java
+++ POO/tp2/Main.java
@@ -1,6 +1,8 @@
 public class Main{
 
     public static void main(String[] args) {
+
+        /*
         Point p1 = new Point(8,6);
         Point p2 = new Point(5,2);
         Segment s1 = new Segment(p1,p2);
@@ -17,5 +19,29 @@ public class Main{
         Triangle t1 = new Triangle(p1,p2,p3);
         System.out.println(t1.perimetre());
         System.out.println(t1.aire());
+        */
+
+        Matrice Mat1 =new Matrice(2,3);
+        Mat1.setValue(0,0, 1);
+        Mat1.setValue(0,1, 2);
+        Mat1.setValue(0,2, 0);
+        Mat1.setValue(1,0, 4);
+        Mat1.setValue(1,1, 3);
+        Mat1.setValue(1,2,-1);
+        Mat1.afficheMatrice();
+        Matrice Mat2 = new Matrice(3,2);
+        Mat2.setValue(0,0, 5);
+        Mat2.setValue(0,1, 1);
+        Mat2.setValue(1,0, 2);
+        Mat2.setValue(1,1, 3);
+        Mat2.setValue(2,0, 3);
+        Mat2.setValue(2,1, 4);
+        Mat2.afficheMatrice();
+
+        Mat1.produit(Mat2).afficheMatrice();
+
+
     }
+
+
 }
--- POO/tp2/Matrice.java
+++ POO/tp2/Matrice.java
@@ -7,14 +7,83 @@ public class Matrice{
     public Matrice (int nbL, int nbC){
         nbLignes = nbL;
         nbCol = nbC;
+
         coefficients = new int[nbLignes][nbCol];
         for (int i =0; i<nbLignes ;++i ) {
-            for (int j = 0; i<nbCol ;++i ) {
+            for (int j = 0; j<nbCol ;++j ) {
                 coefficients[i][j] = 0;
             }
         }
     }
 
-    
+    public Matrice (int [][] coef){
+        coefficients = coef;
+    }
+
+    public int getNbLignes(){
+        return nbLignes;
+    }
+
+    public int getNbCol(){
+        return nbCol;
+    }
+
+    public void setNbLignes(int nbL){
+        nbLignes = nbL;
+    }
+
+    public void setNbCol(int nbC){
+        nbCol = nbC;
+    }
+
+    //public static litMatrice(){}
+    public void afficheMatrice(){
+        for (int i = 0; i < nbLignes ;++i ) {
+            for (int j = 0; j<nbCol ; ++j ) {
+                System.out.print(coefficients[i][j]+", ");
+            }
+            System.out.print('\n');
+        }
+    }
+    public int getValue(int Lig, int Col){
+        return coefficients[Lig][Col];
+    }
+    public void setValue(int Lig, int Col, int v){
+        coefficients[Lig][Col] = v;
+    }
+
+    public Matrice addition(Matrice m){
+        Matrice resultat = new Matrice(nbLignes, nbCol);
+
+        for (int i = 0; i < nbLignes ;++i ) {
+            for (int j = 0; j<nbCol ; ++j ) {
+                resultat.setValue(i, j, this.getValue(i,j) + m.getValue(i,j) );
+            }
+        }
+        return resultat;
+    }
+
+
+    public Matrice produit(Matrice m){
+        Matrice resultat = new Matrice(nbLignes, m.getNbCol());
+        int lig = resultat.getNbLignes();
+        int col = resultat.getNbCol();
+        int sum;
+        for (int i = 0; i < lig ;++i ) {
+            for (int j = 0; j< col; ++j ) {
+                sum = 0;
+                for (int k = 0; k < col  ; ++k ) {
+                    sum += this.getValue(i, k) * m.getValue( k, j);
+                }
+                resultat.setValue(i, j, sum);
+            }
+        }
+        return resultat;
+    }
+
+
+
+
+
 
 }

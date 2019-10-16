92b9059324200c8d980a83d063f2a31489ba0d45
playmaker <chaoleicai@gmail.com>
2019-10-16 18:16:32 +0000

    creat tp4

---
+++ MachinesParalleles/tp4/count.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <unistd.h>
+#include <mutex>
+#include <stdlib.h>
+#include <chrono>
+#include <condition_variable>
+
+
+/*
+    mots de 2 characteres no need
+    pas de nombre
+    
+*/
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}

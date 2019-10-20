c1f036c88667333d83740b02ea45a4e289d03d78
playmaker <chaoleicai@gmail.com>
2019-10-20 22:14:15 +0000

    debut tp, tree a faire

--- MachinesParalleles/tp4/count.cpp
+++ MachinesParalleles/tp4/count.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <thread>
-#include <vector>
-#include <unistd.h>
 #include <mutex>
-#include <stdlib.h>
-#include <chrono>
-#include <condition_variable>
-
+#include <fstream>
+#include <string>
 
 /*
     mots de 2 characteres no need
     pas de nombre
-    
+
 */
 int main(int argc, char const *argv[]) {
+    std::string line;
+    char c;
+    if(argc == 2){
+        std::ifstream myfile(argv[1]);
+        if(myfile.is_open()){
 
+            while (myfile.get(c)) {
+                std::cout << c;
+            }
+            myfile.close();
+        }
+    }
+    else{
+        std::cout << "Usage: ./executable Filename" << '\n';
+    }
     return 0;
 }

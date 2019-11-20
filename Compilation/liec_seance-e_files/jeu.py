62774898d1b2787c66570fc3341e3d93216bd4c3
playmaker <chaoleicai@gmail.com>
2019-11-20 10:27:44 +0100

    creation tp e

---
+++ Compilation/liec_seance-e_files/jeu.py
@@ -0,0 +1,20 @@
+prng_state = 42
+
+def prng (sup):
+    global prng_state
+    prng_state = (prng_state * 1664525 + 1013904223) % 2**32
+    return prng_state % sup
+
+def jeu (max):
+    print('Le nombre est entre 0 et ' + str(max))
+    n = prng(max)
+    guess = -1
+    while guess != n:
+        guess = int(input('Entrez un nombre: '))
+        if guess > n:
+            print("Trop grand.")
+        elif guess < n:
+            print("Trop petit.")
+    print("Bravo :).")
+
+jeu(300)

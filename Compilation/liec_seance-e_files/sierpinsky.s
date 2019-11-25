14a8d26c7dbe20ee04363e0b525e43abe79dc318
playmaker <chaoleicai@gmail.com>
2019-11-25 15:42:58 +0000

    creation fichier.s

---
+++ Compilation/liec_seance-e_files/sierpinsky.s
@@ -0,0 +1,33 @@
+.text
+.globl main
+
+main:
+  li $v0, 4
+  la $a0, num1q
+  syscall
+
+  li $v0, 5
+  syscall
+  move $t2, $v0
+
+loop:
+  beq $t2, $0, end_loop
+  add $t1, $t1, $t2
+  addi $t2, $t2, -1
+
+  b loop
+end_loop:
+  li $v0, 4
+  la $a0, sum
+  syscall
+
+  move $a0, $t1
+  li $v0, 1
+  syscall
+
+  jr $ra
+
+.data
+num1q: .asciiz "Please enter the number N : "
+sum:   .asciiz "The sum of these numbers is: "
+nl:    .asciiz "\n"

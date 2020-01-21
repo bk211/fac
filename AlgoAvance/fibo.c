8d2ae915a25ddf4bcf7a8a2108c61208eba6042e
playmaker <chaoleicai@gmail.com>
2020-01-21 12:11:00 +0100

    add timer

--- AlgoAvance/fibo.c
+++ AlgoAvance/fibo.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <math.h>
+
 typedef unsigned long long int ullint;
 
 struct paire
@@ -36,4 +42,64 @@ ullint fibo (int n) {
 	return res.fun;
   }
   return (ullint) 0;
+}
+
+ullint fiborec(int n){
+    if(n < 2){
+        return 1;
+    }
+    return fiborec(n-1) + fiborec(n-2);
+}
+
+ullint fiboiter(int n){
+    ullint a = 1;
+    ullint b =1;
+    int c;
+    for (int i = 2; i <= n; i++)
+    {
+        c = a + b;
+        b = a;
+        a = c;
+    }
+    return a;
+}
+
+int main(int argc, char const *argv[])
+{
+    int tps_init, tps_crt, dtps;
+    int i;
+    int max = 82;
+    
+    tps_init = (int) clock();
+    for ( i = 0; i < max ; i++)
+    {
+        //fiborec(i);
+    }
+    tps_crt = (int) clock();
+    dtps = tps_crt - tps_init;
+    printf("Tps pour fiborec %d\n",dtps);
+
+
+    tps_init = (int) clock();
+    for ( i = 0; i < max ; i++)
+    {
+        fiboiter(i);
+    }
+    tps_crt = (int) clock();
+    dtps = tps_crt - tps_init;
+    printf("Tps pour fiboiter %d\n",dtps);    
+
+
+    tps_init = (int) clock();
+    for ( i = 0; i < max ; i++)
+    {
+        fiblog(i);
+    }
+    tps_crt = (int) clock();
+    dtps = tps_crt - tps_init;
+    printf("Tps pour fibolog %d\n",dtps);
+    
+
+
+    return 0;
 }
